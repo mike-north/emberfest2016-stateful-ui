00068edd4fc2934da7c1cf6859d038d43d8c5e87
Mike North <michael.l.north@gmail.com>
2016-10-24 00:47:44 +0200

    1-navigation

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,7 +3,22 @@ import Ember from 'ember';
 const { Route } = Ember;
 
 export default Route.extend({
-  model() {
-    return this.store.findAll('post');
+  queryParams: {
+    search: {
+      as: 's',
+      replace: true
+    }
+  },
+  actions: {
+    pageRefresh() {
+      this.refresh();
+    }
+  },
+  model({ search }) {
+    if (!search) {
+      return this.store.findAll('post');
+    } else {
+      return this.store.query('post', { search });
+    }
   }
 });
