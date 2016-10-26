a8634cb8043086a5c553d4fe7dea26e128f3fb25
Mike North <michael.l.north@gmail.com>
2016-10-26 15:27:50 +0200

    Persisted state

--- app/components/post-full.js
+++ app/components/post-full.js
@@ -5,5 +5,15 @@ const { Component } = Ember;
 
 export default Component.extend({
   classNames: ['post-full'],
-  postInfo: stateFor('post-info', 'model')
+  postInfo: stateFor('post-info', 'model'),
+  actions: {
+    deleteComment(commentRecord) {
+      commentRecord.destroyRecord();
+    },
+    persistCommentDraft() {
+      let commentBody = this.get('postInfo.body');
+      let post = this.get('model');
+      this.attrs['save-comment'](post, commentBody);
+    }
+  }
 });
--- app/controllers/posts/show.js
+++ app/controllers/posts/show.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
 
 const { Controller } = Ember;
 
 export default Controller.extend({
-
+  actions: {
+    saveComment(post, commentBody) {
+      this.store.createRecord('comment', {
+        post,
+        body: commentBody
+      }).save().then(() => {
+        // delete the draft so we're clean again
+        stateFor('post-info') // WeakMap for all post-infos
+          .get(post) // post-info for the post we commented on
+          .set('body', ''); // set the draft comment back to ''
+      });
+    }
+  }
 });
